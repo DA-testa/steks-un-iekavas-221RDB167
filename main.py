7beac89a82c1de0e55a6b64c4f6349bef8cde190
221RDB167 <124889522+221RDB167@users.noreply.github.com>
2023-02-19 21:11:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,57 @@
+# python3
+# Renars Misuns 8.grupa 221RDB167
+
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
+
+
+
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
+
+
+
 
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i))
-            pass
+            opening_brackets_stack.append(Bracket(next, i + 1))
 
-        if next in ")]}":
-            
+
+        elif next in ")]}":
             if not opening_brackets_stack:
                 return i + 1
-                
             top = opening_brackets_stack.pop()
-
             if not are_matching(top.char, next):
                 return i + 1
-            pass
-
     if opening_brackets_stack:
-        return opening_brackets_stack[0].position + 1
-
-    return 0
+        return opening_brackets_stack[0].position
+    else:
+        return "Success"
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    if mismatch == 0:
-        print("Success")
+    type = input().strip()
+    if type.isdigit() and int(type) in range(6):
+        number = type
+        path = "/workspaces/steks-un-iekavas-221RDB167/test/" + number
+        with open (path, "r") as f:
+            text = f.read().strip()
+    elif type == "I":
+        text = input("Enter brackets: ")
     else:
-        print(mismatch)
+        print("Invalid input type")
+        return
+
+
+    mismatch = find_mismatch(text)
+    print(mismatch)
+
+
+
 
 if __name__ == "__main__":
     main()
